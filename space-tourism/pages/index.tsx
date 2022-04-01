2cdf81c9acb613da84be60a6f2c84c44506330d7
Guilherme Popolin <guilhermespopolin@gmail.com>
2022-04-01 18:04:07 +0200

    feat(space-tourism): initial commit

---
+++ space-tourism/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Space Tourism</title>
+        <meta
+          name="description"
+          content="Frontend mentor Space Tourism challange"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Space Tourism</h1>
+    </>
+  );
+};
+
+export default Home;
