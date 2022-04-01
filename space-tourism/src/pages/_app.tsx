d4a5b1ada1399febc52410bceb0a09e7ad522fbf
Guilherme Popolin <guilhermespopolin@gmail.com>
2022-04-01 18:55:01 +0200

    feat(space-tourism): added theme support

--- space-tourism/src/pages/_app.tsx
+++ space-tourism/src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+import { theme } from "@/styles/theme";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
---
+++ space-tourism/src/styles/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    hsl: {
+      primary: "230 35% 7%",
+      accent: "231 77% 90%",
+      white: "0 0% 100%",
+    },
+  },
+  typography: {},
+};
+
+export type Theme = typeof theme;
