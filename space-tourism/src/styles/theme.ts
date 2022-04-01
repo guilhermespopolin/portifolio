d209fb4f4a8172a9de9d3723a6dfb85648dccc6b
Guilherme Popolin <guilhermespopolin@gmail.com>
2022-04-01 20:06:50 +0200

    feat(space-tourism): added typography into theme

--- space-tourism/src/styles/theme.ts
+++ space-tourism/src/styles/theme.ts
@@ -6,7 +6,23 @@ export const theme = {
       white: "0 0% 100%",
     },
   },
-  typography: {},
+  typography: {
+    fontFamily: {
+      serif: '"Bellefair", serif',
+      sansNormal: '"Barlow", sans-serif',
+      sansCond: '"Barlow Condensed", sans-serif',
+    },
+    fontSize: {
+      900: "9.375rem",
+      800: "6.25rem",
+      700: "3.5rem",
+      600: "2rem",
+      500: "1.75rem",
+      400: "1.125rem",
+      300: "1rem",
+      200: "0.875rem",
+    },
+  },
 };
 
 export type Theme = typeof theme;
