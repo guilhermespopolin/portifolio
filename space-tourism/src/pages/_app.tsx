f1736a553d4120e18b33e38d857acf834c6ba2c3
Guilherme Popolin <guilhermespopolin@gmail.com>
2022-04-05 02:22:24 +0200

    refact(space-tourism): remove styled-components

--- space-tourism/src/pages/_app.tsx
+++ space-tourism/src/pages/_app.tsx
@@ -1,18 +1,9 @@
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "styled-components";
 
-import { GlobalStyle } from "@/components/GlobalStyle";
-
-import { theme } from "@/styles/theme";
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
