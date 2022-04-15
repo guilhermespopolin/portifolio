1bd10ce6d5c8a884fccb1b3294525ddd34889dcd
Guilherme Popolin <guilhermespopolin@gmail.com>
2022-04-15 16:22:46 +0200

    refact(space-tourism): rename - cover layout

--- space-tourism/src/components/CoverLayout/CoverLayout.tsx
+++ space-tourism/src/components/CoverLayout/CoverLayout.tsx
@@ -1,14 +1,16 @@
 import type { ReactNode } from "react";
+import cx from "classnames";
 
 import { Navigation } from "@/components/Navigation";
 
-export type MainLayoutProps = {
+export type CoverLayoutProps = {
+  className?: string;
   children: ReactNode;
 };
 
-export function MainLayout({ children }: MainLayoutProps) {
+export function CoverLayout({ className, children }: CoverLayoutProps) {
   return (
-    <>
+    <div className={cx("h-screen grid grid-rows-[auto_1fr]", className)}>
       <header>
         <Navigation
           links={[
@@ -17,7 +19,7 @@ export function MainLayout({ children }: MainLayoutProps) {
           ]}
         />
       </header>
-      <main>{children}</main>
-    </>
+      <main className="overflow-y-auto">{children}</main>
+    </div>
   );
 }
