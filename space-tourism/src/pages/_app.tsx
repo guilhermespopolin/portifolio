b82469f799706607fe714f19400c699af10ff4ec
Guilherme Popolin <guilhermespopolin@gmail.com>
2022-04-15 10:49:12 +0200

    feat(space-tourism): added meta tags for pages

--- space-tourism/src/pages/_app.tsx
+++ space-tourism/src/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
+import Head from "next/head";
+
 import "@/styles/globals.css";
 
 type NextPageWithLayout = NextPage & {
@@ -15,7 +17,28 @@ type AppPropsWithLayout = AppProps & {
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width"
+          initial-scale="1.0"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon-32x32.png"
+        />
+
+        <title>Frontend Mentor | Space Tourism Website</title>
+      </Head>
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  );
 }
 
 export default MyApp;
