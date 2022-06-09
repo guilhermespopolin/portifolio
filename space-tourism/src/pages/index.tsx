4e07d7d8bbc676f6f86f61a98e5a88d6193aede8
Guilherme Popolin <guilhermespopolin@gmail.com>
2022-06-09 17:42:18 +0200

    styles(home): updated

--- space-tourism/src/pages/index.tsx
+++ space-tourism/src/pages/index.tsx
@@ -1,27 +1,75 @@
-import type { NextPage } from "next";
+import type { NextPageWithLayout } from "next";
 
 import Head from "next/head";
+import { CoverLayout } from "@/components/CoverLayout";
 import { Container } from "@/components/Container";
-import { Heading } from "@/components/Heading";
+import { ExploreLink } from "@/components/ExploreLink";
 
-const Home: NextPage = () => {
+const HomePage: NextPageWithLayout = () => {
   return (
     <>
       <Head>
         <title>Space Tourism</title>
-        <meta
-          name="description"
-          content="Frontend mentor Space Tourism challange"
-        />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container className="py-4 md:py-8">
-        <Heading className="text-center" level={1}>
-          Space Tourism
-        </Heading>
+      <Container
+        className={["h-full", "grid", "items-center", "md:grid-cols-2"].join(
+          " "
+        )}
+      >
+        <div>
+          <h1
+            className={[
+              "font-sansCond",
+              "uppercase",
+              "text-accent-500",
+              "text-center",
+              "tracking-widest",
+              "md:text-xl",
+              "md:text-left",
+            ].join(" ")}
+          >
+            So, you want to travel to
+            <span
+              className={[
+                "block",
+                "text-white",
+                "font-serif",
+                "text-7xl",
+                "my-5",
+                "md:text-9xl",
+              ].join(" ")}
+            >
+              Space
+            </span>
+          </h1>
+          <p className="text-accent-500 text-center md:text-justify">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className="justify-self-center md:justify-self-end">
+          <ExploreLink>Explore</ExploreLink>
+        </div>
       </Container>
     </>
   );
 };
 
-export default Home;
+HomePage.getLayout = (page) => (
+  <CoverLayout
+    className={[
+      "bg-cover",
+      "bg-bottom",
+      "bg-[url('/assets/home/background-home-mobile.jpg')]",
+      "md:bg-[url('/assets/home/background-home-tablet.jpg')]",
+      "xl:bg-[url('/assets/home/background-home-desktop.jpg')]",
+      "xl:bg-right-bottom",
+    ].join(" ")}
+  >
+    {page}
+  </CoverLayout>
+);
+
+export default HomePage;
